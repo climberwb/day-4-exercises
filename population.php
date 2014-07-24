b8c88410e624abe6f380dc431e0515a3c6f8f5e6
Warren Kushner <warren.kushner@gmail.com>
2014-07-24 21:54:59 +0000

    finally finished population!

--- population.php
+++ population.php
@@ -1,26 +1,53 @@
 <?php
-$city = $_GET['city'];
+$connection = mysql_connect('127.0.0.1', 'root', '');
+mysql_select_db('day_4_database');
+//$ cityArray = mysql_query('SELECT city_name, population FROM population')
+// mysql_connect(‘SQLdata.com’, ‘Jones’, ‘un1c0rn’, ‘my_database’);
+
+// Check connection
+if (!$connection) {
+  die('Unable to connect: ' . mysql_errno());
+}
+
+
+#
 ?>
 <h1>Welcome to Population Data Online</h1>
 <?
-$cities = array(
-  'Philadelphia' => 1548000,
-  'New York' => 8337000,
-  'Los Angeles' => 3858000,
-  'Seattle' => 634535,
-  'Boston' => 636479,
-);
-if ($city && isset($cities[$city])) {
-  print '<p>The population of <strong>' . $city . '</strong> is <strong>' . $cities[$city] . '</strong></p>';
+//$cities = array(
+ // 'Philadelphia' => 1548000,
+ // 'New York' => 8337000,
+ // 'Los Angeles' => 3858000,
+ // 'Seattle' => 634535,
+ // 'Boston' => 636479,
+//);
+if ($connection) {
+if(!isset($_GET['city'])){
+print 'No city fount';
 }
+else{
+$city = $_GET['city'];
+print $city;
+$result = mysql_query('SELECT city_name, population FROM population WHERE city_name = "'.$city.'"');
+while($row = mysql_fetch_array($result)){
 
+print'<p> the population is'  . $row['city_name'] .'and '.$row['population'];
+//$result = mysql_query('SELECT city_name, population FROM population where city_name = '.$city);
+//  print '<p>The population of <strong>' . $row['city_name'] . '</strong> is <strong>' . $row['population'] . '</strong></p>';
+}
+}
+}
 ?>
 <h2>Our Cities</h2>
 <ul>
 <?php
-  foreach ($cities as $name => $value) {
-    print '<li><a href="/jody/population.php?city=' . $name  . '">' . $name
+$result = mysql_query('SELECT city_name, population FROM population');
+if($connection){
+//$row = mysql_fetch_array($result)
+  while ($row = mysql_fetch_array($result)) {
+    print '<li><a href="/day-4-exercises/population.php?city=' . $row['city_name']  . '">' . $row['city_name']
     . '</a></li>';
   }
+}
 ?>
 </ul>
